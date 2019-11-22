09173e0c9f9677fc4b7b8b38ba40d6dbb94b1496
SuhYPark <52521529+SuhYPark@users.noreply.github.com>
2019-11-22 10:09:39 -0500

    Add files via upload

---
+++ darkMode/daynight/dark.js
@@ -0,0 +1,9 @@
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('.toggle').click(function(){
+        $('.toggle').toggleClass('active')
+        $('body').toggleClass('night')
+    })
+})
+</script>
