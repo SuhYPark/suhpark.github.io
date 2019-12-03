55ebd5ea3619b92b2859d34657c730507d2a9c44
SuhYPark <52521529+SuhYPark@users.noreply.github.com>
2019-12-03 18:15:56 -0500

    update 12/3/19

--- darkMode/daynight/dark.js
+++
@@ -1,9 +0,0 @@
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script type="text/javascript">
-  $(document).ready(function(){
-    $('.toggle').click(function(){
-        $('.toggle').toggleClass('active')
-        $('body').toggleClass('night')
-    })
-})
-</script>
